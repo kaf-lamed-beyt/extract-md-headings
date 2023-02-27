7e4cc1c09d0ba7d3fabc0fe05a48cdb53a8d1fe9
kaf-lamed-beyt <belac335@gmail.com>
2023-02-27 14:22:28 +0100

    feat: add id property to heading objects

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import fs from "fs"
+import fs from "fs";
 
 interface Heading {
+  id: number;
   slug: string;
   title: string;
   level: number;
@@ -9,18 +10,20 @@ interface Heading {
 /**
  * @description gets all the heading-text from a markdown file
  * @param {string} filePath  a valid relative path, as a string to where the markdown file is located.
- * @returns an array of objects containing the heading information like the `title`, `slug`, and `level`
+ * @returns an array of objects containing the heading information like the `id`, `title`, `slug`, and `level`
  * @see [Documentation](https://github.com/kaf-lamed-beyt/extract-md-headings#usage)
  */
+
 export function extractHeadings(filePath: string): Array<Heading> {
-    const content = fs.readFileSync(filePath, "utf-8")
-    const headings: Array<Heading> = [];
+  const content = fs.readFileSync(filePath, "utf-8");
+  const headings: Array<Heading> = [];
 
   // match the `#` syntax for headings
   const headingMatcher = /^(#+)\s(.+)$/gm;
 
   let match = headingMatcher.exec(content);
   while (match !== null) {
+    const id = Math.floor(Math.random() * 900000) + 100000;
     const level = match[1].length;
     const title = match[2].trim();
     const slug = title
@@ -28,7 +31,7 @@ export function extractHeadings(filePath: string): Array<Heading> {
       .replace(/[^\w\s-]/g, "")
       .replace(/\s+/g, "-");
 
-    headings.push({ slug, title, level });
+    headings.push({ id, slug, title, level });
     match = headingMatcher.exec(content);
   }
 
