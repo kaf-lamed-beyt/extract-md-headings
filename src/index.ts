567666e7cc5287aa7e38092b9fbb534275205e1a
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2024-02-26 20:51:09 +0200

    Add support for custom slugFn, add simple test suite

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,26 @@ interface Heading {
   level: number;
 }
 
+export interface ExtractHeadingsOptions {
+  slugFn?: (title: string) => string;
+}
+
 /**
+ * @typedef ExtractHeadingsOptions
+ * @type {object}
+ * @property {function} slugFn - a function to convert the heading title to a slug, if you wish to override the default slug generation algorithm
+ *
  * @description gets all the heading-text from a markdown file
  * @param {string} filePath  a valid relative path, as a string to where the markdown file is located.
+ * @param {ExtractHeadingsOptions} options an optional configuration object to customize the behaviour of the function
  * @returns an array of objects containing the heading information like the `id`, `title`, `slug`, and `level`
  * @see [Documentation](https://github.com/kaf-lamed-beyt/extract-md-headings#usage)
  */
 
-export function extractHeadings(filePath: string): Array<Heading> {
+export function extractHeadings(
+  filePath: string,
+  options?: ExtractHeadingsOptions
+): Array<Heading> {
   const content = fs.readFileSync(filePath, "utf-8");
   const headings: Array<Heading> = [];
 
@@ -26,10 +38,8 @@ export function extractHeadings(filePath: string): Array<Heading> {
     const id = Math.floor(Math.random() * 900000) + 100000;
     const level = match[1].length;
     const title = match[2].trim();
-    const slug = title
-      .toLowerCase()
-      .replace(/[^\w\s-]/g, "")
-      .replace(/\s+/g, "-");
+    const slugFn = options?.slugFn ?? defaultSlugFn;
+    const slug = slugFn(title);
 
     headings.push({ id, slug, title, level });
     match = headingMatcher.exec(content);
@@ -37,3 +47,10 @@ export function extractHeadings(filePath: string): Array<Heading> {
 
   return headings;
 }
+
+const defaultSlugFn = (title: string): string => {
+  return title
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-");
+};
